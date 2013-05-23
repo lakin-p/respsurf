ee2633abee8843cefe2866d94f92403fe2e3d4cb
Paul Lakin <prl26@case.edu>
2013-05-23 12:06:29 -0400

    Now have a working demo.

--- R/respsurf-package.R
+++ R/respsurf-package.R
@@ -22,10 +22,14 @@
 #' 
 #' data(ups.data)
 #' demofit.up <- rsFit(data  = ups.data,
-#'                        start = list(B = 0.12, Econ = 1.19, EC50.1 = 1.4, 
-#'                                     EC50.2 = 3.4, m1 = 2.0, m2 = 2.1, 
-#'                                     alfa = 2.0),
-#'                        mpos = TRUE
+#'                     start = list(B = 0.12, Econ = 1.19, EC50.1 = 1.4,
+#'                                  EC50.2 = 3.4, m1 = 2.0, m2 = 2.1,
+#'                                  alfa = 2.0),
+#'                     lower = list(B = 0, Econ = 0, EC50.1 = .0000001, EC50.2 = .0000001,
+#'                                  m1 = .1, m2 = .1, alfa = .1),
+#'                     upper = list(B = 1, Econ = 5, EC50.1 = 20, EC50.2 = 20,
+#'                                  m1 = 6, m2 = 6, alfa = 50),
+#'                     mpos = TRUE
 #' )
 #' summary(demofit.up)
 #' plot(demofit.up)
